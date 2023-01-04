fb475657a051830f2fd9b8a71e3c3253cb592214
Arpun Nagaraja <arnagaraja@gmail.com>
2023-01-04 13:30:14 -0600

    Adding a couple pages to the "website" app

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse("Welcome to the Meeting Planner")
+
+def about(request):
+    aboutMe = "Learning Django via PluralSight"
+    return HttpResponse(aboutMe)
