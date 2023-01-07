0fd9d90043fb9a678a1f402bb8dbf926f7d60622
Arpun Nagaraja <arnagaraja@gmail.com>
2023-01-06 22:22:49 -0600

    Pulling objects from the DB and using template language

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from meetings.models import Meeting
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner")
+    return render(request, "website/welcome.html",
+        {"num_meetings": Meeting.objects.count()})
 
 def about(request):
     aboutMe = "Learning Django via PluralSight"
