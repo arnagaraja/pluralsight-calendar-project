a93f355573e5a3bcc2f85b69e1c97a579f0dae99
Arpun Nagaraja <arnagaraja@gmail.com>
2023-01-06 23:20:39 -0600

    Room list, template for loops, url mappings

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -5,7 +5,7 @@ from meetings.models import Meeting
 # Create your views here.
 def welcome(request):
     return render(request, "website/welcome.html",
-        {"num_meetings": Meeting.objects.count()})
+        {"meetings": Meeting.objects.all()})
 
 def about(request):
     aboutMe = "Learning Django via PluralSight"
