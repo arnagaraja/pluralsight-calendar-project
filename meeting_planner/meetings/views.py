a0510013f085bbbfe06630d7d82e3a34711f538d
Arpun Nagaraja <arnagaraja@gmail.com>
2023-01-07 14:47:28 -0600

    CSS, template inheritance, forms

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from meetings.models import Meeting, Room
+from django.forms import modelform_factory
 
 # Create your views here.
 def detail(request, id):
@@ -8,3 +9,15 @@ def detail(request, id):
 
 def rooms(request):
     return render(request, "meetings/rooms.html", {"rooms": Room.objects.all()})
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
